4d681ac0fe81cc046866fa8bae6597535d2a444a
Brett Zamir <brettz9@yahoo.com>
2013-09-07 17:09:37 +0800

    reduce nesting of utility functions

--- polyfills/XMLSerializer.js
+++ polyfills/XMLSerializer.js
@@ -17,10 +17,67 @@ var XMLSerializer;
     }
     // Todo: Make this configurable whether to always add?
     if (1 || !XMLSerializer.prototype.serializeToString) {
-        var emptyElements = '|basefont|frame|isindex'+ // Deprecated
+        var prohibitHTMLOnly = true,
+            emptyElements = '|basefont|frame|isindex'+ // Deprecated
             '|area|base|br|col|command|embed|hr|img|input|keygen|link|meta|param|source|track|wbr|',
             nonEmptyElements = 'article|aside|audio|bdi|canvas|datalist|details|figcaption|figure|footer|header|hgroup|mark|meter|nav|output|progress|rp|rt|ruby|section|summary|time|video' + // new in HTML5
-            'html|body|p|h1|h2|h3|h4|h5|h6|form|button|fieldset|label|legend|select|option|optgroup|textarea|table|tbody|colgroup|tr|td|tfoot|thead|th|caption|abbr|acronym|address|b|bdo|big|blockquote|center|code|cite|del|dfn|em|font|i|ins|kbd|pre|q|s|samp|small|strike|strong|sub|sup|tt|u|var|ul|ol|li|dd|dl|dt|dir|menu|frameset|iframe|noframes|head|title|a|map|div|span|style|script|noscript|applet|object|';
+            'html|body|p|h1|h2|h3|h4|h5|h6|form|button|fieldset|label|legend|select|option|optgroup|textarea|table|tbody|colgroup|tr|td|tfoot|thead|th|caption|abbr|acronym|address|b|bdo|big|blockquote|center|code|cite|del|dfn|em|font|i|ins|kbd|pre|q|s|samp|small|strike|strong|sub|sup|tt|u|var|ul|ol|li|dd|dl|dt|dir|menu|frameset|iframe|noframes|head|title|a|map|div|span|style|script|noscript|applet|object|',
+            pubIdChar = /^(\u0020|\u000D|\u000A|[a-zA-Z0-9]|[\-'()+,.\/:=?;!*#@$_%])*$/,
+            xmlChars = /([\u0009\u000A\u000D\u0020-\uD7FF\uE000-\uFFFD]|[\uD800-\uDBFF][\uDC00-\uDFFF])*$/,
+            entify = function entify(str) { // FIX: this is probably too many replaces in some cases and a call to it may not be needed at all in some cases
+                return str.replace(/&/g, '&amp;').replace(/>/g, '&gt;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
+            },
+            clone = function clone (obj) { // We don't need a deep clone, so this should be sufficient without recursion
+                var prop, newObj = {};
+                for (prop in obj) {
+                    if (obj.hasOwnProperty(prop)) {
+                        newObj[prop] = obj[prop];
+                    }
+                }
+                return JSON.parse(JSON.stringify(newObj));
+            },
+            invalidStateError = function () { // These are probably only necessary if working with text/html
+                if (prohibitHTMLOnly) {
+                    // INVALID_STATE_ERR per section 9.3 XHTML 5: http://www.w3.org/TR/html5/the-xhtml-syntax.html
+                    throw window.DOMException && DOMException.create ?
+                        DOMException.create(11) :
+                        // If the (nonstandard) polyfill plugin helper is not loaded (e.g., to reduce overhead and/or modifying a global's property), we'll throw our own light DOMException
+                        {message: 'INVALID_STATE_ERR: DOM Exception 11', code: 11};
+                }
+            },
+            addExternalID = function (node, all) {
+                if (node.systemId.indexOf('"') !== -1 && node.systemId.indexOf("'") !== -1) {
+                    invalidStateError();
+                }
+                var string = '',
+                    publicId = node.publicId,
+                    systemId = node.systemId,
+                    publicQuote  = publicId && publicId.indexOf("'") !== -1 ? "'" : '"', // Don't need to check for quotes here, since not allowed with public
+                    systemQuote  = systemId && systemId.indexOf("'") !== -1 ? "'" : '"'; // If as "entity" inside, will it return quote or entity? If former, we need to entify here (should be an error per section 9.3 of http://www.w3.org/TR/html5/the-xhtml-syntax.html )
+                if (systemId !== null && publicId !== null) {
+                    string += ' PUBLIC ' + publicQuote + publicId + publicQuote + ' ' + systemQuote + systemId + systemQuote;
+                }
+                else if (publicId !== null) {
+                    string += ' PUBLIC ' + publicQuote + publicId + publicQuote;
+                }
+                else if (all || systemId !== null) {
+                    string += ' SYSTEM ' + systemQuote + systemId + systemQuote;
+                }
+                return string;
+            },
+            notIEInsertedAttributes = function (att, node, nameVals) {
+                return nameVals.every(function (nameVal) {
+                    var name = Array.isArray(nameVal) ? nameVal[0] : nameVal,
+                        val = Array.isArray(nameVal) ? nameVal[1] : null;
+                    return att.name !== name ||
+                        (val && att.value !== val) ||
+                        //(!node.outerHTML.match(new RegExp(' ' + name + '=')));
+                        (node.outerHTML.match(new RegExp(' ' + name + '=' + val ? '"' + val + '"' : '')));
+                });
+            },
+            lowerCaseCSSPropertiesForIE = function (n0, n1) {
+                return n1.toLowerCase() + ' ';
+            };
 
         XMLSerializer.prototype.serializeToString = function (nodeArg) {
 
@@ -33,76 +90,21 @@ var XMLSerializer;
                 htmlMode = true, // Todo: Make conditional on namespace?
                 emptyElement,
                 namespaces = {},
-                prohibitHTMLOnly = true,
                 xmlDeclaration = true,
                 string = '',
                 children = {},
-                i = 0,
-                xmlChars = /([\u0009\u000A\u000D\u0020-\uD7FF\uE000-\uFFFD]|[\uD800-\uDBFF][\uDC00-\uDFFF])*$/,
-                entify = function entify(str) { // FIX: this is probably too many replaces in some cases and a call to it may not be needed at all in some cases
-                    return str.replace(/&/g, '&amp;').replace(/>/g, '&gt;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
-                },
-                clone = function clone (obj) { // We don't need a deep clone, so this should be sufficient without recursion
-                    var prop, newObj = {};
-                    for (prop in obj) {
-                        if (obj.hasOwnProperty(prop)) {
-                            newObj[prop] = obj[prop];
-                        }
-                    }
-                    return JSON.parse(JSON.stringify(newObj));
-                },
-                invalidStateError = function () { // These are probably only necessary if working with text/html
-                    if (prohibitHTMLOnly) {
-                        // INVALID_STATE_ERR per section 9.3 XHTML 5: http://www.w3.org/TR/html5/the-xhtml-syntax.html
-                        throw window.DOMException && DOMException.create ?
-                            DOMException.create(11) :
-                            // If the (nonstandard) polyfill plugin helper is not loaded (e.g., to reduce overhead and/or modifying a global's property), we'll throw our own light DOMException
-                            {message: 'INVALID_STATE_ERR: DOM Exception 11', code: 11};
-                    }
-                },
-                addExternalID = function (node, all) {
-                    if (node.systemId.indexOf('"') !== -1 && node.systemId.indexOf("'") !== -1) {
-                        invalidStateError();
-                    }
-                    var publicId = node.publicId, systemId = node.systemId,
-                        publicQuote  = publicId && publicId.indexOf("'") !== -1 ? "'" : '"', // Don't need to check for quotes here, since not allowed with public
-                        systemQuote  = systemId && systemId.indexOf("'") !== -1 ? "'" : '"'; // If as "entity" inside, will it return quote or entity? If former, we need to entify here (should be an error per section 9.3 of http://www.w3.org/TR/html5/the-xhtml-syntax.html )
-                    if (systemId !== null && publicId !== null) {
-                        string += ' PUBLIC '+publicQuote+publicId+publicQuote+' '+systemQuote+systemId+systemQuote;
-                    }
-                    else if (publicId !== null) {
-                        string += ' PUBLIC '+publicQuote+publicId+publicQuote;
-                    }
-                    else if (all || systemId !== null) {
-                        string += ' SYSTEM '+systemQuote+systemId+systemQuote;
-                    }
-                };
-            function notIEInsertedAttributes (att, node, nameVals) {
-                return nameVals.every(function (nameVal) {
-                    var name = Array.isArray(nameVal) ? nameVal[0] : nameVal,
-                        val = Array.isArray(nameVal) ? nameVal[1] : null;
-                    return att.name !== name ||
-                        (val && att.value !== val) ||
-                        //(!node.outerHTML.match(new RegExp(' ' + name + '=')));
-                        (node.outerHTML.match(new RegExp(' ' + name + '=' + val ? '"' + val + '"' : '')));
-                });
-            }
-            function lowerCaseCSSPropertiesForIE (n0, n1) {
-                return n1.toLowerCase() + ' ';
-            }
+                i = 0;
+
             function serializeDOM(node, namespaces) {
-                var string = '';
+                var children, tagName, tagAttributes, prefix, val, content, i,
+                    string = '',
+                    nodeValue = node.nodeValue,
+                    type = node.nodeType;
                 namespaces = clone(namespaces) || {}; // Ensure we're working with a copy, so different levels in the hierarchy can treat it differently
 
                 if ((node.prefix && node.prefix.indexOf(':') !== -1) || (node.localName && node.localName.indexOf(':') !== -1)) {
                     invalidStateError();
                 }
-
-                var nodeValue = node.nodeValue,
-                    type = node.nodeType,
-                    i = 0,
-                    children, tagName, tagAttributes, prefix, val, content,
-                    pubIdChar = /^(\u0020|\u000D|\u000A|[a-zA-Z0-9]|[\-'()+,.\/:=?;!*#@$_%])*$/;
 
                 if (
                         ((type === 3 || type === 4 || type === 7 || type === 8) &&
@@ -268,7 +270,7 @@ var XMLSerializer;
                         }
                         string += '<'+'!ENTITY '+node.nodeName+' ';
                         if (node.publicId || node.systemId) { // External Entity?
-                            addExternalID(node);
+                            string += addExternalID(node);
                             if (node.notationName) {
                                 string += ' NDATA '+node.notationName;
                             }
@@ -314,7 +316,7 @@ var XMLSerializer;
                         if (!pubIdChar.test(node.publicId)) {
                             invalidStateError();
                         }
-                        addExternalID(node);
+                        string += addExternalID(node);
                         string += node.internalSubset ? '[\n' + node.internalSubset + '\n]' : '';
                         string += '>\n';
                         /* Fit in internal subset along with entities?: probably don't need as these would only differ if from DTD, and we're not rebuilding the DTD
@@ -331,7 +333,7 @@ var XMLSerializer;
                         break;
                     case 12: // NOTATION (would need to be passed in directly)
                         string += '<'+'!NOTATION '+node.nodeName;
-                        addExternalID(node, true);
+                        string += addExternalID(node, true);
                         string += '>';
                         break;
                     default:
