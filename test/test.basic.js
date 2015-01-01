7f7932355247c8b164a612d970cabb2370a52af0
Brett Zamir <brettz9@yahoo.com>
2015-01-01 16:05:39 -0700

    add text node test

--- test/test.basic.js
+++ test/test.basic.js
@@ -5,23 +5,35 @@
 var jml = require('../jml'),
     testCase = require('nodeunit').testCase,
     DOMParser = require('xmldom').DOMParser,
-    XMLSerializer = require('xmldom').XMLSerializer;
+    XMLSerializer = require('xmldom').XMLSerializer,
+    jsdom = require('jsdom').jsdom;
 
 var xml = new DOMParser().parseFromString('<div class="test">someContent</div>', 'text/html');
 
+var document = jsdom('');
+var window = document.parentWindow;
 
 module.exports = testCase({
 
     // ============================================================================
-    'simple div': function(test) {
+    'element with content': function(test) {
     // ============================================================================
         test.expect(1);
         var expected = ['div', {'class': 'test', 'xmlns': 'http://www.w3.org/1999/xhtml'}, ['someContent']];
         var result = jml.toJML(xml.documentElement);
         test.deepEqual(expected, result);
         test.done();
-    }
+    },
+    // ============================================================================
+    'text node': function(test) {
+    // ============================================================================
+        test.expect(1);
+        var expected = 'text node content';
 
+        var result = jml.toJML(document.createTextNode(expected));
+        test.deepEqual(expected, result);
+        test.done();
+    }
 });
 
 }());
