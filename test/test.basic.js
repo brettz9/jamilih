4e107ae9f89835e8b50a24e0e020eefd4c1d3df9
Brett Zamir <brettz9@yahoo.com>
2015-01-01 16:56:58 -0700

    Clean-up testing, add doc fragment test, add dependency to own xmldom fork (to avoid dispatchEvent errors upon child appending with xmldom Element object in jsdom)

--- test/test.basic.js
+++ test/test.basic.js
@@ -8,7 +8,10 @@ var jml = require('../jml'),
     XMLSerializer = require('xmldom').XMLSerializer,
     jsdom = require('jsdom').jsdom;
 
-var xml = new DOMParser().parseFromString('<div class="test">someContent</div>', 'text/html'); // This polyfill apparently mistakenly avoids putting the document into a full HTML document (with body)
+var xml = new DOMParser().parseFromString('<div class="test">someContent</div>', 'text/html');
+var divDOM = xml.documentElement; // This polyfill apparently mistakenly avoids putting the document into a full HTML document (with body)
+
+var divJamilih = ['div', {'class': 'test', 'xmlns': 'http://www.w3.org/1999/xhtml'}, ['someContent']];
 
 var document = jsdom('');
 var window = document.parentWindow;
@@ -19,8 +22,8 @@ module.exports = testCase({
     'element with text content': function(test) {
     // ============================================================================
         test.expect(1);
-        var expected = ['div', {'class': 'test', 'xmlns': 'http://www.w3.org/1999/xhtml'}, ['someContent']];
-        var result = jml.toJML(xml.documentElement);
+        var expected = divJamilih;
+        var result = jml.toJML(divDOM);
         test.deepEqual(expected, result);
         test.done();
     },
@@ -31,6 +34,16 @@ module.exports = testCase({
         var expected = 'text node content';
 
         var result = jml.toJML(document.createTextNode(expected));
+        test.deepEqual(expected, result);
+        test.done();
+    },
+    // ============================================================================
+    'document fragment': function (test) {
+    // ============================================================================
+        var expected = {'#': [divJamilih]};
+        var frag = document.createDocumentFragment();
+        frag.appendChild(divDOM.cloneNode(true));
+        var result = jml.toJML(frag);
         test.deepEqual(expected, result);
         test.done();
     }
