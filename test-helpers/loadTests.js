96628531b7a58b5a67beb395cb42d3761eb2b14d
Brett Zamir <brettz9@yahoo.com>
2019-12-07 00:22:22 +0800

    - Testing: Move code out of HTML to bootstrap file

--- test-helpers/loadTests.js
+++ test-helpers/loadTests.js
@@ -1,5 +1,8 @@
-/* globals chai */
+/* globals mocha, chai */
 import {jml, glue, nbsp} from '../src/jml.js';
+
+mocha.setup('bdd');
+mocha.globals(['jml', 'glue', 'nbsp']);
 
 window.jml = jml;
 window.glue = glue;
