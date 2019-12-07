4a0879a7897e02d54ef0ecc8564167de94de6ec2
Brett Zamir <brettz9@yahoo.com>
2019-12-07 08:18:21 +0800

    - Testing: Ignore for coverage

--- src/jml-polyglot.js
+++ src/jml-polyglot.js
@@ -1,6 +1,7 @@
 /* eslint-env node */
 import {jml, $, $$, nbsp, body, glue} from './jml.js';
 
+/* istanbul ignore else */
 if (typeof process !== 'undefined') {
   // import {JSDOM} from 'jsdom';
   const {JSDOM} = require('jsdom'); // eslint-disable-line global-require
